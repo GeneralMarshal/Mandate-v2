b22127e8a5d7fcf749b1fa7539d62eb5d0325cab
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2024-08-17 00:18:07 +0100

    bottom nav component for mobile sizes intialized, later today i'll the onclick functions

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,6 +3,8 @@ import Filter from "../Components/Filter.jsx";
 import Footer from "../Components/Footer.jsx";
 import Main from "../Components/Main.jsx";
 import HeaderMain from "../Components/HeaderMain.jsx";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { CgProfile } from "react-icons/cg";
 
 
 
@@ -16,6 +18,23 @@ export default function LandingPage() {
       <Filter />
       <Main />
       <Footer links={true} />
+      <nav className=" flex justify-center gap-[40px] p-[10px] fixed bottom-0 left-0 w-full bg-[#ffff] z-20">
+        <div className="flex flex-col items-center">
+          <FaMagnifyingGlass className=" text-[blue] text-[22px]" />
+          <p>Trending</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <img
+            src="Assets/icons/thin-heart.svg"
+            className="filter-icon w-[22px]"
+          />
+          <p>Trending</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <CgProfile className=" text-[22px]" />
+          <p>Trending</p>
+        </div>
+      </nav>
     </>
   );
 }
