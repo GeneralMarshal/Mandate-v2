7edff7fce4c9072ace736c1e1c3c96782347f87e
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2024-08-17 14:00:30 +0100

    linked mobile bottom nav to login page and explore(scroll up)

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -5,12 +5,21 @@ import Main from "../Components/Main.jsx";
 import HeaderMain from "../Components/HeaderMain.jsx";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { CgProfile } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
 
 
 
 
 
 export default function LandingPage() {
+  const navigate = useNavigate()
+  function exploreHandler(){
+      window.scrollTo({ top: 0, behavior: "smooth" });
+
+  }
+  function loginHandler(){
+    navigate("/login")
+  }
   return (
     <>
       <HeaderMain search={true} />
@@ -18,21 +27,21 @@ export default function LandingPage() {
       <Filter />
       <Main />
       <Footer links={true} />
-      <nav className=" flex justify-center gap-[40px] p-[10px] fixed bottom-0 left-0 w-full bg-[#ffff] z-20">
-        <div className="flex flex-col items-center">
+      <nav className="  flex md:hidden justify-center gap-[40px] p-[10px] fixed bottom-0 left-0 w-full bg-[#ffff] z-20">
+        <div className=" flex flex-col items-center cursor-pointer" onClick={ ()=>exploreHandler()} >
           <FaMagnifyingGlass className=" text-[blue] text-[22px]" />
           <p>Trending</p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center cursor-pointer">
           <img
             src="Assets/icons/thin-heart.svg"
             className="filter-icon w-[22px]"
           />
           <p>Trending</p>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center cursor-pointer" onClick={ ()=>loginHandler()}>
           <CgProfile className=" text-[22px]" />
-          <p>Trending</p>
+          <p>Login</p>
         </div>
       </nav>
     </>
